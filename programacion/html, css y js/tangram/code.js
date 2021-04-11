00656cdd5099e39ad20058b4c1875f689753f6fc
Andrés Aldana <aldanasilva91@gmail.com>
2021-04-10 19:45:43 -0500

    Adición pato al tangram en html, css, js

--- programacion/html, css y js/tangram/code.js
+++ programacion/html, css y js/tangram/code.js
@@ -39,7 +39,6 @@ btn.addEventListener('click', () => {
     cambiarPosicion('pato');
 });
 
-<<<<<<< HEAD
 /* Para crear más figuras
  * 
  * - Crear botón en plantilla html con id="<nombre de la figura>"
@@ -48,8 +47,5 @@ btn.addEventListener('click', () => {
  * 
 */
 
-=======
-//cambiarPosicion('pato');
->>>>>>> 6cb5bd73601330b9f79a1b7fd8d4cb542d0f2ee8
 
 
